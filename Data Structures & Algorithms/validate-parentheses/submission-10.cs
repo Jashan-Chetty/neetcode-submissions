e3ef4e0ce8ed87e62a93c5d2d46e5d7e54f88288
JASHAN CHETTY <jashanchetty1611@gmail.com>
2026-08-20 17:48:37 +0200

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public bool IsValid(string s) {
+        Stack<char> par = new();
+
+        foreach(char c in s) {
+            if(c == '{') par.Push('}');
+            else if(c == '(') par.Push(')');
+            else if(c == '[') par.Push(']');
+            else if(par.Count == 0 || par.Pop() != c) return false;
+
+            
+        }
+
+        return par.Count == 0;
+    }
+}
