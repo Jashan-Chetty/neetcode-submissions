bd3bdd1d7aea12c19bf7b615334166619a42a1e7
JASHAN CHETTY <jashanchetty1611@gmail.com>
2026-09-08 18:14:27 +0200

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0, right = nums.Length - 1;
+
+        while(left <= right) {
+            int midpoint = left + (right - left) / 2;
+
+            if(nums[midpoint] == target) {
+                return midpoint;
+            }
+            else if(nums[midpoint] < target ){
+                left = midpoint + 1;
+            }
+            else {
+                right = midpoint - 1;
+            }
+        }
+
+        return -1;
+    }
+}
