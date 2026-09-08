2e09db9e2e06b942816e69a787208523c2dd4a71
JASHAN CHETTY <jashanchetty1611@gmail.com>
2026-09-08 11:03:23 +0200

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+
+        Dictionary<int, int> seen = new();
+
+        for(int i = 0; i < nums.Length; i++) {
+            int difference = target - nums[i];
+
+            if(seen.ContainsKey(difference)) {
+                return new int[] { seen[difference], i };
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return null;
+
+    }
+}
